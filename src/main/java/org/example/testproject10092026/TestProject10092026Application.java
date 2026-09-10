d91da610e3535c124e8f60111c217e8162ac50fe
TOKJ <tokj0001@stud.ek.dk>
2026-09-10 08:38:20 +0200

    test

---
+++ src/main/java/org/example/testproject10092026/TestProject10092026Application.java
@@ -0,0 +1,13 @@
+package org.example.testproject10092026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject10092026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProject10092026Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/testproject10092026/TestProject10092026ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testproject10092026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProject10092026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
