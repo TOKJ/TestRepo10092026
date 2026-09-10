ac41c93d2130a9fdda021c6fee6fa273835334c8
TOKJ <tokj0001@stud.ek.dk>
2026-09-10 08:56:27 +0200

    tests oprettet!

---
+++ src/test/java/Test1.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+    @Test
+    void test1FEJLER() {
+        int a = 2;
+        int b = 3;
+        assertEquals(7, a+b);
+    }
+}
